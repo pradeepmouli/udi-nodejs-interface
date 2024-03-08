e6dbc25b23c41f3f46068fe595415dd4ffbcdde3
Pradeep Mouli <pmouli@mac.com>
2024-03-08 14:10:55 -0500

    Added index.ts

---
+++ src/index.ts
@@ -0,0 +1,20 @@
+'use strict';
+
+export function useCloud() {
+  return process.env.MQTTENDPOINT && process.env.STAGE;
+
+};
+import Node from './Node.js';
+import logger from './logger.js';
+
+export { Interface } from './Interface.js';
+// If we are connecting to Polyglot Cloud, use the PGC interface instead;
+export  default useCloud() ? require('pgc_interface') : {
+  // Interface class for Polyglot
+  Interface: await import('./Interface.js'),
+  // Node class from which all nodes are extended from
+  Node: await import('./Node.js'),
+
+  // Logger utility for the NodeServer (Entries will be tagged with NS:)
+  logger: (await import('./logger.js')).ns,
+};
